1bffa1f7c566906be26b442cbdf55f75337774f5
Resuragam <1791614500@qq.com>
2022-11-27 16:09:05 +0800

    :lipstick: default page navbar

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,18 +1,28 @@
+<script setup lang="ts">
+const route = useRoute()
+const router = useRouter()
+const activeIndex = ref(route.path)
+const handleSelect = (key: string, keyPath: string[]) => {
+  router.push(key)
+}
+</script>
+
 <template>
   <header>
-    <div class="header-navbar">
-      <div class="flex items-center">
-        <img src="https://hnust-ticknet-swzl.oss-cn-hangzhou.aliyuncs.com/secondClass/2021/11/05/4c9bc131-9503-4708-b528-e19b66f0eb80.webp" alt="" width="40" height="40" />
-        <span>
-          TickNet
-        </span>
-      </div>
-      <div class="flex">
-        <span>
-          123
-        </span>
-      </div>
-    </div>
+    <el-menu
+      :default-active="activeIndex"
+      mode="horizontal"
+      :ellipsis="false"
+      @select="handleSelect"
+    >
+      <el-menu-item index="0">
+        <img src="https://hnust-ticknet-swzl.oss-cn-hangzhou.aliyuncs.com/secondClass/2021/11/05/4c9bc131-9503-4708-b528-e19b66f0eb80.webp" alt="" width="58" height="58"/>
+      </el-menu-item>
+      <div class="flex-grow" />
+      <el-menu-item index="/">首页</el-menu-item>
+      <el-menu-item index="/team">团队介绍</el-menu-item>
+      <el-menu-item index="/process">招新流程</el-menu-item>
+    </el-menu>
   </header>
   <main>
     <RouterView />
@@ -20,16 +30,17 @@
 </template>
 
 <style scoped>
-.header-navbar {
-  display: flex;
-  height: 4rem;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 6rem;
+:deep(.el-menu) {
+  padding: 0 15vw;
+}
+.flex-grow {
+  /*flex-grow: 1;*/
+}
+.logo {
+  width:58px;
+  height: 58px;
 }
 @media screen and (max-width: 768px) {
-  .header-navbar {
-    padding: 0 2rem
-  }
+
 }
 </style>
